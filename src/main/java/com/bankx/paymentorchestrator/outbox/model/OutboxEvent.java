9f7ae5604ff23ad4f3035e7d27c73a1423c4ac99
P-Lidia <Lydiasmolyakova@gmail.com>
2025-11-10 01:17:42 +0100

    feature: 10.11.2025 Create OutboxEvent class and database migration for outbox_events table

--- src/main/java/com/bankx/paymentorchestrator/outbox/model/OutboxEvent.java
+++ src/main/java/com/bankx/paymentorchestrator/outbox/model/OutboxEvent.java
@@ -5,11 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class OutboxEvent {
+    private UUID id;
+    private String requestId;
+    private String eventType;  // для расширяемость, пока что всегда будет "TRANSFER_CREATED"
+    private String payload;
+    private OutboxStatus status;
+    private Instant createAt;
+    private Instant updateAt;
+    private int retryCount;
+    private String errorMessage;  // Для ошибок при отправке (для дебага)
 
+    // todo в сервисе платежа надо будет добавить String payload = objectMapper.writeValueAsString(request); - сериализация в JSON
+    /* payload - это те данные, которые потом пойдут в kafkaEvent. */
 
 }
